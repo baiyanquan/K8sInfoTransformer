2b16f249e4b0251a2d2aaa2e57a82d551808f579
1854039 <45162720+1854039@users.noreply.github.com>
2021-04-11 19:25:23 +0800

    Revert "写好了第二个接口"
    This reverts commit 288e136ae5674380defe223a3d22e4be0cd6c179.

--- controller/return_json_controller.py
+++ controller/return_json_controller.py
@@ -1,18 +1,11 @@
 from flask import Flask, jsonify, abort, request
-import time
-from service.k8s_data_transformer import K8sDataTransformer
 
 app = Flask(__name__)
 
 
-@app.route('/get_json', methods=['GET'])
+@app.route('/hello', methods=['GET'])
 def get_tasks():
-    time.time()
-    # simplified_time = time.strftime("%Y%m%d_%H%M%S")
-    simplified_time = "20210314_111500"
-    print(simplified_time)
-    k8s_data_transformer = K8sDataTransformer()
-    return k8s_data_transformer.transform_k8s_data(simplified_time)
+    return 'hello'
 
 
 if __name__ == '__main__':
