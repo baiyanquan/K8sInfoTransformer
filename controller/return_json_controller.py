7932ae42f3fd1f277b155eab8cf620a6491fa037
1854039 <45162720+1854039@users.noreply.github.com>
2021-04-19 14:41:44 +0800

    存整条数据

--- controller/return_json_controller.py
+++ controller/return_json_controller.py
@@ -1,24 +1,33 @@
 from flask import Flask, jsonify, abort, request
-import time
 from service.k8s_data_transformer import K8sDataTransformer
+import json
+import  time
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
 
+mongo=PyMongo(app,uri="mongodb://mongoadmin:mongoadmin@10.60.38.173:27017/templatedata?authSource=admin")
+
+@app.route('/store_data', methods=['GET'])
+def check():
+    simplified_time = "20210215_155600"
+    k8s_data_transformer = K8sDataTransformer()
+    res = json.dumps(k8s_data_transformer.transform_k8s_data(simplified_time))
+    data = json.loads(res)
+    mongo.db.test1.insert(data)
+    return res
+
 
 @app.route('/get_json', methods=['GET'])
-def get_json():
+def get_tasks():
     time.time()
-    # 本地时间
     # simplified_time = time.strftime("%Y%m%d_%H%M%S")
     simplified_time = "20210314_111500"
     print(simplified_time)
     k8s_data_transformer = K8sDataTransformer()
     return k8s_data_transformer.transform_k8s_data(simplified_time)
 
-
-@app.route('/hello', methods=['GET'])
-def hello():
-    return 'hello'
 
 
 if __name__ == '__main__':
